81dda478aca5ef9a2b077e3aecc6773855d03d42
SophYo <sophiya.dimitrova@gmail.com>
2019-07-07 15:24:55 +0300

    Initial Commit: Encapsulation/ Lab Exercises

---
+++ src/Main.java
@@ -0,0 +1,93 @@
+import person.Person;
+import team.Team;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader =
+                new BufferedReader(
+                        new InputStreamReader(
+                                System.in
+                        )
+                );
+
+        int n = Integer.parseInt(reader.readLine());
+
+        printValidationData(reader, n);
+
+//        printSortedPeople(reader, n);
+
+
+    }
+
+    private static void printSortedPeople(BufferedReader reader, int n) throws IOException {
+        List<Person> people = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] input = reader.readLine().split(" ");
+            people.add(new Person(input[0], input[1], Integer.parseInt(input[2])));
+        }
+
+        people.sort((firstPerson, secondPerson) -> {
+            int sComp = firstPerson.getFirstName().compareTo(secondPerson.getFirstName());
+
+            if (sComp != 0) {
+                return sComp;
+            } else {
+                return Integer.compare(firstPerson.getAge(), secondPerson.getAge());
+            }
+        });
+
+        for (Person person : people) {
+            System.out.println(person.toString());
+        }
+    }
+
+    private static void printValidationData(BufferedReader reader, int n) throws IOException {
+        List<Person> people = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] input = reader.readLine().split(" ");
+            try {
+                people
+                        .add(
+                                new Person(
+                                        input[0],
+                                        input[1],
+                                        Integer.parseInt(input[2]),
+                                        Double.parseDouble(input[3])));
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+            }
+
+        Team team= new Team("Black Eagles");
+
+        people.forEach(team::addPlayer);
+
+        System.out.println(String.format("First team have %d players",
+                team.getFirstTeam().size()));
+
+        System.out.println((String.format("Reserve team have %d players",
+                team.getReserveTeam().size())));
+
+//        for (Person person : people) {
+//            team.addPlayer(person);
+//        }
+
+//        double bonus = Double.parseDouble(reader.readLine());
+//        for (Person person : people) {
+//            try {
+//                person.increaseSalary(bonus);
+//                System.out.println(person.toString());
+//            } catch (IllegalArgumentException e) {
+//                System.out.println(e.getMessage());
+//            }
+//        }
+    }
+}
---
+++ src/person/Person.java
@@ -0,0 +1,92 @@
+package person;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double salary;
+
+    public Person(String firstName, String lastName, int age) {
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.setAge(age);
+    }
+
+    public Person(String firstName, String lastName, int age, double salary) {
+        this(firstName, lastName, age);
+        this.setSalary(salary);
+    }
+
+    public void setFirstName(String firstName) {
+        if (firstName.length() < 3) {
+            throw new IllegalArgumentException("First name cannot be less than 3 symbols");
+        }
+
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        if (lastName.length() < 3) {
+            throw new IllegalArgumentException("Last name cannot be less than 3 symbols");
+        }
+
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        if (age < 1) {
+            throw new IllegalArgumentException("Age cannot be zero or negative integer");
+        }
+        this.age = age;
+    }
+
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+     double getSalary() {
+        return salary;
+    }
+
+    private void setSalary(double salary) {
+        if (salary < 460) {
+            throw new IllegalArgumentException("Salary cannot be less than 460 leva");
+        }
+
+        this.salary = salary;
+    }
+
+    public void increaseSalary(double bonus) {
+        if (this.getAge() < 30) {
+           this.setSalary((this.getSalary()) + (this.getSalary() * bonus/ 200));
+        } else {
+            this.setSalary((this.getSalary()) + (this.getSalary() * bonus/ 100));
+
+        }
+
+    }
+
+    protected String getFullName() {
+        return this.getFirstName() + " " + this.getLastName();
+    }
+
+    @Override
+    public String toString() {
+//        return String.format("%s is %d years old",
+//                this.getFullName(),
+//                this.getAge());
+
+        return String.format("%s gets %.1f leva",
+                this.getFullName(),
+                this.getSalary());
+    }
+}
